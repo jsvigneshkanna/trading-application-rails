69e01d1090dc80b6af605e523742888d16ef3796
jsvigneshkanna <vkrishitaran@gmail.com>
2025-08-11 09:31:07 +0530

    Remove unused library entries from project configuration

--- Makefile
+++
@@ -1,5 +0,0 @@
-rails:
-	docker compose run --rm trading_api rails $(CMD)
-
-migrate:
-	docker compose run --rm trading_api rails db:migrate
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Remove a potentially pre-existing server.pid
-rm -f /app/tmp/pids/server.pid
-
-# Execute the container's main process (what's set as CMD in Dockerfile)
-exec "$@"
